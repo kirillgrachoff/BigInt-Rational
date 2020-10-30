5eb3a1e12bea12247d53ca0dff925bfe3c7ce2ff
Kirill <kirill.grachoff@gmail.com>
2020-10-30 23:57:13 +0300

    initial commit

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include "biginteger.h"
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using std::cin;
+using std::cout;
+using std::vector;
+using std::string;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+
+template<class T>
+constexpr T max() {
+    return std::numeric_limits<T>::max();
+}
+
+template<class T>
+constexpr T min() {
+    return std::numeric_limits<T>::min();
+}
+
+template <typename T>
+void runTest(T v) {
+    cout << v << '\n';
+}
+
+bool checkTest(int a, int b) {
+    if (a + b != BigInteger(a) + BigInteger(b)) {
+        cout << a << ' ' << b << ' ' << a + b << " ~ " << BigInteger(a) + BigInteger(b) << '\n';
+        return false;
+    }
+    return true;
+}
+
+void solve() {
+    cout << Rational(1, -70).asDecimal(10) << '\n';
+}
+
+int main() {
+    solve();
+}
